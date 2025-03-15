491eee92ff3eb8c2cd57b30e4f02835085859931
Koen Seeuws <168718548+koen-seeuws@users.noreply.github.com>
2025-03-15 17:46:51 +0100

    Implemented DomainEvent Dispatching

---
+++ TheCodeKitchen/TheCodeKitchen.Infrastructure.SignalR/SignalRServiceRegistration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TheCodeKitchen.Infrastructure.SignalR.Services;
+
+namespace TheCodeKitchen.Infrastructure.SignalR;
+
+public static class SignalRServiceRegistration
+{
+    public static IServiceCollection AddSignalRServices(this IServiceCollection services)
+    {
+        services.AddSignalR();
+
+        services.AddSingleton<IRealtimeGameManagementService, GameManagementSignalRService>();
+        services.AddSingleton<IRealtimeKitchenManagementService, KitchenManagementSignalRService>();
+        
+        return services;
+    }
+}
