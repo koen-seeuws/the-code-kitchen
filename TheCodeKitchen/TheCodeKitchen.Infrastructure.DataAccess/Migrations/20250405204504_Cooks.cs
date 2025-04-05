592099664b7a51e5775fb09e33de2e1fb2bdd337
Koen Seeuws <168718548+koen-seeuws@users.noreply.github.com>
2025-04-05 23:00:54 +0200

    Working JoinGame

---
+++ TheCodeKitchen/TheCodeKitchen.Infrastructure.DataAccess/Migrations/20250405204504_Cooks.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TheCodeKitchen.Infrastructure.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class Cooks : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cook",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PasswordHash = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    KitchenId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Created = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    Modified = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cook", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Cook_Kitchens_KitchenId",
+                        column: x => x.KitchenId,
+                        principalTable: "Kitchens",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cook_KitchenId",
+                table: "Cook",
+                column: "KitchenId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cook");
+        }
+    }
+}
