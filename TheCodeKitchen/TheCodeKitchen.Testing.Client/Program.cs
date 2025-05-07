c3fc24355842106c8977d81366290f8b0f86cb92
Koen Seeuws <168718548+koen-seeuws@users.noreply.github.com>
2025-05-07 22:47:48 +0200

    Added very basic Client project for testing

---
+++ TheCodeKitchen/TheCodeKitchen.Testing.Client/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.SignalR.Client;
+using TheCodeKitchen.Application.Contracts.Events;
+using TheCodeKitchen.Application.Contracts.Models;
+
+const string apiUrl = "http://localhost:5169/";
+const string kitchenCode = "";
+
+var apiClient = new HttpClient { BaseAddress = new Uri(apiUrl) };
+
+//Auth
+var authRequest = new AuthenticationRequest("Koen", "Test123!", kitchenCode);
+var httpResponse = await apiClient.PostAsJsonAsync("game/join", authRequest);
+httpResponse.EnsureSuccessStatusCode();
+var response = await httpResponse.Content.ReadFromJsonAsync<AuthenticationResponse>();
+ArgumentNullException.ThrowIfNull(response);
+ArgumentException.ThrowIfNullOrWhiteSpace(response.Token);
+
+apiClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", response.Token);
+
+//SignalR
+var connection = new HubConnectionBuilder()
+    .WithUrl($"{apiUrl}KitchenHub", options =>
+    {
+        options.AccessTokenProvider = () => Task.FromResult(response.Token)!;
+    })
+    .Build();
+
+connection.On<NextMomentEvent>(nameof(NextMomentEvent), nextMomentEvent =>
+{
+    Console.WriteLine($"Kitchen: {nextMomentEvent.KitchenId} - Moment: {nextMomentEvent.Moment}");
+});
