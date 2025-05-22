590621c58517dbbaa5550bd427b1d3fddd897314
Koen Seeuws <168718548+koen-seeuws@users.noreply.github.com>
2025-05-23 00:15:47 +0200

    Initializing Equipment

--- TheCodeKitchen/TheCodeKitchen.Application.Business/Grains/KitchenGrain/KitchenGrain.Initialize.cs
+++ TheCodeKitchen/TheCodeKitchen.Application.Business/Grains/KitchenGrain/KitchenGrain.Initialize.cs
@@ -1,4 +1,6 @@
+using TheCodeKitchen.Application.Contracts.Grains.Equipment;
 using TheCodeKitchen.Application.Contracts.Requests;
+using TheCodeKitchen.Application.Contracts.Requests.Equipment;
 
 namespace TheCodeKitchen.Application.Business.Grains.KitchenGrain;
 
@@ -6,9 +8,9 @@ public sealed partial class EquipmentGrain
 {
     public async Task<Result<CreateKitchenResponse>> Initialize(CreateKitchenRequest request, int count)
     {
-        if(state.RecordExists)
+        if (state.RecordExists)
             return new AlreadyExistsError($"The kitchen with id {this.GetPrimaryKey()} has already been initialized");
-        
+
         var id = this.GetPrimaryKey();
 
         var name = request.Name?.Trim();
@@ -20,18 +22,26 @@ public sealed partial class EquipmentGrain
 
         if (!codeResult.Succeeded)
             return codeResult.Error;
-        
+
         // State
         var kitchen = new Kitchen(id, name, codeResult.Value, request.GameId);
         state.State = kitchen;
         await state.WriteStateAsync();
-        
+
         // Equipment 
-        
+        foreach (var equipment in state.State.Equipment)
+        {
+            for (var number = 0; number < equipment.Value; number++)
+            {
+                var equipmentGrain = GrainFactory.GetGrain<IEquipmentGrain>(state.State.Id, number.ToString(), equipment.Key);
+                var createEquipmentRequest = new CreateEquipmentRequest(state.State.Game, state.State.Id, number);
+                await equipmentGrain.Initialize(createEquipmentRequest);
+            }
+        }
         
         // Streams
         await SubscribeToNextMomentEvent();
-        
+
         return mapper.Map<CreateKitchenResponse>(kitchen);
     }
 }
