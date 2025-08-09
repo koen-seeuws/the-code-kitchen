615cf81ea31fa4ae0982cd3c8fce5975f1d6d0fb
Koen Seeuws <168718548+koen-seeuws@users.noreply.github.com>
2025-08-09 22:03:01 +0200

    Added [GenerateSerializer] to GameCreatedEvent

--- TheCodeKitchen/TheCodeKitchen.Application.Contracts/Events/GameManagement/GameCreatedEvent.cs
+++ TheCodeKitchen/TheCodeKitchen.Application.Contracts/Events/GameManagement/GameCreatedEvent.cs
@@ -1,3 +1,5 @@
 namespace TheCodeKitchen.Application.Contracts.Events.GameManagement;
 
+[GenerateSerializer]
+
 public record GameCreatedEvent(Guid Id, string Name, double SpeedModifier, double Temperature);
