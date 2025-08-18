6c0c24033f7621b196233075ee55ab40b04b2d3e
Koen Seeuws <168718548+koen-seeuws@users.noreply.github.com>
2025-08-18 23:05:51 +0200

    Fixed KitchenOrder deserialization error

--- TheCodeKitchen/TheCodeKitchen.Core.Domain/KitchenOrder.cs
+++ TheCodeKitchen/TheCodeKitchen.Core.Domain/KitchenOrder.cs
@@ -1,19 +1,30 @@
 namespace TheCodeKitchen.Core.Domain;
 
-public class KitchenOrder(ICollection<OrderFoodRequest>? requestedFoods, long number, Guid game, Guid kitchen)
+public class KitchenOrder
 {
-    public long Number { get; set; } = number;
+    public long Number { get; set; }
     public TimeSpan Time { get; set; } = TimeSpan.Zero;
     public bool Completed { get; set; }
     public double CompletenessRating { get; set; } = 0.0;
 
-    public Guid Game { get; set; } = game;
-    public Guid Kitchen { get; set; } = kitchen;
+    public Guid Game { get; set; }
+    public Guid Kitchen { get; set; }
 
-    public ICollection<KitchenOrderFoodRequest> RequestedFoods { get; set; } =
-        requestedFoods?
-            .Select(fr => new KitchenOrderFoodRequest(fr.Food, fr.MinimumTimeToPrepareFood))
-            .ToList() ?? [];
+    public ICollection<KitchenOrderFoodRequest> RequestedFoods { get; set; }
 
     public ICollection<KitchenOrderFoodDelivery> DeliveredFoods { get; set; } = new List<KitchenOrderFoodDelivery>();
+
+    public KitchenOrder()
+    {
+    }
+
+    public KitchenOrder(ICollection<OrderFoodRequest>? requestedFoods, long number, Guid game, Guid kitchen)
+    {
+        Number = number;
+        Game = game;
+        Kitchen = kitchen;
+        RequestedFoods = requestedFoods?
+            .Select(fr => new KitchenOrderFoodRequest(fr.Food, fr.MinimumTimeToPrepareFood))
+            .ToList() ?? [];
+    }
 }
