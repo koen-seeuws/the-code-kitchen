68621e2dbc0a9337c963baf67731ec40a2f8ef95
Koen Seeuws <168718548+koen-seeuws@users.noreply.github.com>
2025-08-29 00:39:00 +0200

    CookGrain OnNextMoment

--- TheCodeKitchen/TheCodeKitchen.Application.Business/Grains/CookGrain/CookGrain.OnNextMomentEvent.cs
+++ TheCodeKitchen/TheCodeKitchen.Application.Business/Grains/CookGrain/CookGrain.OnNextMomentEvent.cs
@@ -1,5 +1,3 @@
-using Microsoft.Extensions.Logging;
-using TheCodeKitchen.Application.Business.Extensions;
 using TheCodeKitchen.Application.Business.Helpers;
 using TheCodeKitchen.Application.Constants;
 using TheCodeKitchen.Application.Contracts.Events.Cook;
@@ -11,27 +9,30 @@ public sealed partial class CookGrain
 {
     private async Task OnNextMomentEvent(NextMomentEvent nextMomentEvent, StreamSequenceToken _)
     {
-        var timerElapsedTasks = new List<Task>();
-
-        foreach (var timer in state.State.Timers)
-        {
-            if (timer.Time > TimeSpan.Zero)
-                timer.Time -= TheCodeKitchenMomentDuration.Value;
-
-            if (timer.Time > TimeSpan.Zero)
-                continue;
-
-            var @event = new TimerElapsedEvent(timer.Number, timer.Note);
-            var sendTimerElapsedTask = realTimeCookService.SendTimerElapsedEvent(state.State.Username, @event);
-            timerElapsedTasks.Add(sendTimerElapsedTask);
-        }
-
-        await Task.WhenAll(timerElapsedTasks);
-
         if (state.State.Food is not null)
             state.State.Food.Temperature = TemperatureHelper.CalculateNextMomentFoodTemperature(
                 state.State.Food.Temperature, nextMomentEvent.Temperature,
                 TheCodeKitchenRoomTemperatureTransferRate.Value
             );
+
+        var nonElapsedTimers = state.State.Timers
+            .Where(timer => timer.Time > TimeSpan.Zero)
+            .ToArray();
+
+        foreach (var timer in nonElapsedTimers)
+        {
+            timer.Time -= TheCodeKitchenMomentDuration.Value;
+        }
+
+        var timerElapsedTasks = state.State.Timers
+            .Except(nonElapsedTimers)
+            .Select(t =>
+            {
+                var @event = new TimerElapsedEvent(t.Number, t.Note);
+                return realTimeCookService.SendTimerElapsedEvent(state.State.Username, @event);
+            })
+            .ToArray();
+
+        await Task.WhenAll(timerElapsedTasks);
     }
 }
