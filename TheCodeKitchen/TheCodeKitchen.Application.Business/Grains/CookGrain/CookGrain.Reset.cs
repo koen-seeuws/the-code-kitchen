154e964eac4a7362618fdcdff2b322953a3ac272
Koen Seeuws <168718548+koen-seeuws@users.noreply.github.com>
2025-09-08 00:44:00 +0200

    Resetting TimerCounter

--- TheCodeKitchen/TheCodeKitchen.Application.Business/Grains/CookGrain/CookGrain.Reset.cs
+++ TheCodeKitchen/TheCodeKitchen.Application.Business/Grains/CookGrain/CookGrain.Reset.cs
@@ -5,7 +5,9 @@ public partial class CookGrain
     public async Task<Result<TheCodeKitchenUnit>> Reset()
     {
         state.State.Food = null;
+        state.State.MessageCounter = 0;
         state.State.Messages.Clear();
+        state.State.TimerCounter = 0;
         state.State.Timers.Clear();
         await state.WriteStateAsync();
         return TheCodeKitchenUnit.Value;
